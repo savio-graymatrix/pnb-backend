91ef9f322d63ee9a0561c67ac4e66d5f2c61306a
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-07 13:18:53 +0530

    Fix: Updated Document Models

--- pnb/db/data_models/__init__.py
+++ pnb/db/data_models/__init__.py
@@ -1,6 +1,7 @@
 from pnb.db.data_models.Instruction import *
 from pnb.db.data_models.Review import *
 from pnb.db.data_models.File import *
+from pnb.db.data_models.LoanApplication import *
 
 DOCUMENT_MODELS = [
     Instruction,
@@ -8,5 +9,6 @@ DOCUMENT_MODELS = [
     ReviewPoints,
     ReviewSet,
     File,
-    
+    LoanApplication
+
 ]
