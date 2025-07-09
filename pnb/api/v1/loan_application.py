774094525ea3197bc2e27e99e42b6e6b2ccfd6fa
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-09 14:59:57 +0530

    Fix: Updated Review Set to contain Document Checklist

--- pnb/api/v1/loan_application.py
+++ pnb/api/v1/loan_application.py
@@ -3,13 +3,14 @@ from typing import List, Optional
 from pnb.db.data_models import LoanApplication, UpdateLoanApplication 
 from pnb.db.utils import (
     CursorPaginationRequest,
-    CursorPaginationResponse,
-    parse_operator_filter,
+    CursorPaginationResponse
 )
 from datetime import datetime, timezone
 from beanie.operators import Set
 from pnb.langgraph.workflows import GRAPHS
 from pnb.langgraph.agents.credit_agent import CreditAgent
+from pnb.langgraph.structured_output.Credit import Credit
+
 
 router = APIRouter(prefix="/loan_application",tags=["Loan Application"])
 
@@ -101,27 +102,11 @@ async def patch_application(application_id: str, data: UpdateLoanApplication = B
     )
     return loan_application
 
-# @router.post("/review")
-# async def review_loan_application(application: LoanApplication = Body(...)):
-#     #print(application)
-#     config = {"configurable":{"thread_id": application.id,"metadata": application.model_dump()}}
-#     result = GRAPHS['credit'].invoke({
-#             "messages": [],  # Initialize with empty messages
-#             "pan_no": application.pan_no,  # Optional: Initialize state directly
-#             "aadhar_no": application.aadhar_no,
-#             "loan_details": application.documents.loan_application_document,  # Pass loan details directly
-#         },config=config)
-#     # result = GRAPHS['credit'].invoke({"messages":[]})
-#     #print(result["structured_response"])
-#     return result["structured_response"]
-
-@router.post("/review")
+@router.post("/review", response_model=Credit)
 async def review_loan_application(application: LoanApplication = Body(...)):
     #print(application)
     config = {"configurable":{"thread_id": application.id,"metadata": application.model_dump()}}
     result = await CreditAgent.credit_agent({
             "messages": [],  # Initialize with empty messages # Pass loan details directly
         },config=config)
-    # result = GRAPHS['credit'].invoke({"messages":[]})
-    #print(result["structured_response"])
     return result
--- pnb/db/data_models/Review.py
+++ pnb/db/data_models/Review.py
@@ -1,19 +1,30 @@
-from beanie import Document, PydanticObjectId
+from beanie import Document, PydanticObjectId, before_event, Save
 from typing import Optional, Literal, List
 
 
-class ReviewPoints(Document):
-    status: Literal["positive","caution","negative"]
-    statement: str
+REVIEW_STATUSES = ["resolved","rejected"]
 
 class Review(Document):
     alert: str
     title: str
     message: str
-    review_points: List[ReviewPoints]
     review_set_id: PydanticObjectId
     review_comment: str
-    review_status: Literal["pending","resolved","issue"]
+    review_status: Literal["pending","resolved","rejected"] = "pending"
+    
+    @before_event(Save)
+    async def prevent_status_change(self):
+        if self.id is not None:  
+            current = await Review.get(self.id)
+            if current and current.status != "pending" or self.status not in REVIEW_STATUSES:
+                raise ValueError(f"Cannot change status from final state: {current.status}")
+    
+    @before_event(Save)
+    async def restrict_comment_changes(self):
+        if self.id is not None:
+            if self.review_comment and self.review_status in REVIEW_STATUSES:
+                raise ValueError(f"Cannot update comment once resolved or rejected")
+            
 
 class ReviewSet(Document):
     application_id: PydanticObjectId
@@ -21,7 +32,6 @@ class ReviewSet(Document):
 class UpdateReview(Review):
     action: Optional[str]
     title: Optional[str]
-    review_points: Optional[List[ReviewPoints]]
     review_set_id: Optional[PydanticObjectId]
 
 class ReviewSetResponse(ReviewSet):
