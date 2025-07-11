784fa4102e3217840698022957ee5d152f953071
GrayMatrix <itsupport@graymatrix.com>
2025-07-11 14:32:16 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@
 # Optionally run migrations or other setup steps here
 
 # Run the app
+uv venv .venv
+source .venv/bin/activate
 uv sync
 exec uvicorn pnb.main:app --host 0.0.0.0 --port 8000 --workers 2
