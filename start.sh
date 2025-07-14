98a6e4ff2dc2b1e22fbb15ba63737fd6ac31cf7e
Arjun Pathak <arjun.pathak@graymatrix.com>
2025-07-14 10:32:32 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@
 
 # Run the app
 uv venv .venv
-source .venv/bin/activate
+. .venv/bin/activate
 uv pip install --system -r pyproject.toml --compile-bytecode
 exec python -m uvicorn pnb.main:app --host 0.0.0.0 --port 8000 --workers 2
