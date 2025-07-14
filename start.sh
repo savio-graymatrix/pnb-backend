db118b8b1103908897cc24de034246c4021890c6
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-14 11:54:09 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -7,4 +7,5 @@ uv venv .venv
 . .venv/bin/activate
 uv sync
 uv pip install --system -r pyproject.toml --compile-bytecode
+echo $OPENAI_API_KEY
 exec python3 -m uvicorn pnb.main:app --host 0.0.0.0 --port 8000 --workers 2
