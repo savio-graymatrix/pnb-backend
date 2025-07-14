7ddcf46a31bf39669a219503ac9f5b6202647fb5
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-14 06:27:35 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@
 # Run the app
 uv venv .venv
 source .venv/bin/activate
-uv sync
+uv pip install --system -r pyproject.toml --compile-bytecode
 exec python -m uvicorn pnb.main:app --host 0.0.0.0 --port 8000 --workers 2
