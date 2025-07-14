fa03c38258f22391b3b305863c8aac039546b963
Arjun Pathak <arjun.pathak@graymatrix.com>
2025-07-14 18:34:38 +0530

    Fix: allowed all urls

--- pnb/main.py
+++ pnb/main.py
@@ -15,7 +15,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://genapp.pocs.tech"],  
+        allow_origins=["*"],  
         allow_credentials=True,
         allow_methods=["*"],  
         allow_headers=["*"],
