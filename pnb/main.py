9704e0ccb2f49e941a4033571e4402d38c245436
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-14 13:42:37 +0000

    main.py edited online with Bitbucket

--- pnb/main.py
+++ pnb/main.py
@@ -15,7 +15,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  
+        allow_origins=["https://genapp.pocs.tech", "http://localhost:5173"],  
         allow_credentials=True,
         allow_methods=["*"],  
         allow_headers=["*"],
