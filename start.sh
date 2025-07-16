24b1e929bf707ef53f256ace036d4429b01978cd
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-16 06:29:04 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -7,4 +7,5 @@ uv venv .venv
 . .venv/bin/activate
 uv sync
 uv pip install --system -r pyproject.toml --compile-bytecode --no-cache-dir
+rm -rf /root/.cache /tmp/*
 exec python3 -m uvicorn pnb.main:app --host 0.0.0.0 --port 8000 --workers 2 --proxy-headers --forwarded-allow-ips='*'
