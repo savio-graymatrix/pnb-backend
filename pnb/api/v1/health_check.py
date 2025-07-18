db883751f7b426294975c59ae019fc9fbbcb299b
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-18 11:44:42 +0000

    health_check.py edited online with Bitbucket

--- pnb/api/v1/health_check.py
+++ pnb/api/v1/health_check.py
@@ -4,7 +4,7 @@ router = APIRouter(prefix="/health_check", tags=["Health Check"])
 
 @router.get("/")
 async def health_check():
-    result = {'success':1, "message": "OK"}
+    result = {'success':1, "message": "OK", "description": "Health Check Approved"}
     if not result:
         raise HTTPException(status_code=404, detail="Health Check failed")
     return result
