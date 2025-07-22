af0a82a35af64d5874970c028e8677bfc115e580
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-22 10:47:26 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 # Run the app
 apt update -y && apt install -y tesseract-ocr 
 apt-get update -y && apt-get install -y poppler-utils 
-rm -rf /root/.cache && rm -rf /tmp/*
+rm -rf /root/.cache && rm -rf /tmp/* && rm -rf .venv
 uv venv .venv
 . .venv/bin/activate
 uv pip install --system -r pyproject.toml --compile-bytecode --no-cache-dir
