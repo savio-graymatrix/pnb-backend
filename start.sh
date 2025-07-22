596b4f7f2442b28b92d70dc9a929292b86544a90
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-22 12:43:56 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 set -e
+uv venv .venv
+. .venv/bin/activate
+uv sync --no-cache-dir --compiled-bytecode
 
 # Run the app
 exec uvicorn pnb.main:app --host 0.0.0.0 --port 8000 --workers 4 --proxy-headers --forwarded-allow-ips='*'
