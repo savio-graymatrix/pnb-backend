e44ab3f23324188a85dd3548c695900cb3fc55f7
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-22 11:06:36 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -4,8 +4,4 @@
 # Run the app
 apt update -y && apt install -y tesseract-ocr 
 apt-get update -y && apt-get install -y poppler-utils 
-rm -rf /root/.cache && rm -rf /tmp/* && rm -rf .venv
-uv venv .venv
-. .venv/bin/activate
-uv pip install --system -r pyproject.toml --compile-bytecode --no-cache-dir
-exec uvicorn pnb.main:app --host 0.0.0.0 --port 8000 --workers 2 --proxy-headers --forwarded-allow-ips='*'
+exec uvicorn pnb.main:app --host 0.0.0.0 --port 8000 --workers 4 --proxy-headers --forwarded-allow-ips='*'
