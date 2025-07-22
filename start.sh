7ef0abb1b5e8ff9c71545ccf5d6cd246e373ca6a
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-22 11:26:57 +0000

    start.sh edited online with Bitbucket

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
+set -e
 
 # Run the app
-apt update -y && apt install -y tesseract-ocr 
-apt-get update -y && apt-get install -y poppler-utils 
 exec uvicorn pnb.main:app --host 0.0.0.0 --port 8000 --workers 4 --proxy-headers --forwarded-allow-ips='*'
