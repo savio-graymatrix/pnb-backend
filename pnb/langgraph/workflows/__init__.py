679294f3fd13d7985776ef88a78fada3b1132e5b
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-24 12:21:10 +0530

    Feat: Implemented persistence with MongoDBSaver

--- pnb/langgraph/workflows/__init__.py
+++ pnb/langgraph/workflows/__init__.py
@@ -1,18 +1,21 @@
-from pnb import LOGGER
+from pnb import LOGGER, SETTINGS
 import os
 from pnb.langgraph.workflows.project_graph import setup_project_graph
 from pnb.langgraph.workflows.credit_graph import setup_credit_graph
 from pnb.langgraph.workflows.chatbot_graph import setup_chatbot_graph
+from pnb.db.stores.MongoStore import MONGO_STORE
+from langgraph.checkpoint.mongodb.aio import AsyncMongoDBSaver
 
 GRAPHS = dict()
 
 
 async def setup_graphs():
+    checkpointer = AsyncMongoDBSaver(MONGO_STORE.client,db_name=SETTINGS.DB_NAME)
     GRAPHS.update(
         {
-            "project": await setup_project_graph(),
-            "credit": await setup_credit_graph(),
-            "chatbot": await setup_chatbot_graph(),
+            "project": await setup_project_graph(checkpointer=checkpointer),
+            "credit": await setup_credit_graph(checkpointer=checkpointer),
+            "chatbot": await setup_chatbot_graph(checkpointer=checkpointer),
         }
     )
     for key, graph in GRAPHS.items():
