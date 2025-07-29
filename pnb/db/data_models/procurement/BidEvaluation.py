16640baf767643e250ceed36ce1ca553465d85e1
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-29 12:47:10 +0530

    Chore: Initialized BidEvaluation Model and APIs

---
+++ pnb/db/data_models/procurement/BidEvaluation.py
@@ -0,0 +1,8 @@
+from beanie import Document, Link
+from datetime import datetime, timezone
+from pnb.db.data_models import Tender
+from typing import Optional
+
+
+class BidEvaluation(Document):
+    pass
