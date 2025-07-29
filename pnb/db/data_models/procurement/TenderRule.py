e94115d25085f7b513fb45b7abbee6522b0e8006
Savio Fernando <savio.fernando@graymatrix.com>
2025-07-29 12:39:03 +0530

    Feat: Implemented Tender Rule APIs and Models

---
+++ pnb/db/data_models/procurement/TenderRule.py
@@ -0,0 +1,23 @@
+from beanie import Document, Link
+from pydantic import Field, BaseModel
+from datetime import datetime, timezone
+from pnb.db.data_models import Tender
+from typing import Optional
+from typing import List
+
+class TenderRule(Document):
+    content: str = Field()
+    tender: Link[Tender]
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    
+    class Settings:
+        name = "tender_rule"
+
+class UpdateTenderRule(TenderRule):
+    content: Optional[str] = Field()
+    # created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: Optional[datetime] = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+class TenderRuleSetStructureedOutput(BaseModel):
+    rules : List[str] = Field(description="List of Rules")
