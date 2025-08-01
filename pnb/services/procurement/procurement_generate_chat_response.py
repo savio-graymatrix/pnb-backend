ffdbd0ba43023c226d4f22f1cd80733bf72338bf
Savio Fernando <savio.fernando@graymatrix.com>
2025-08-01 12:36:22 +0530

    Fix: Allowed Chatbot to respond without checkpointer

--- pnb/services/procurement/procurement_generate_chat_response.py
+++ pnb/services/procurement/procurement_generate_chat_response.py
@@ -17,54 +17,54 @@ def serialise_ai_message_chunk(chunk):
 async def generate_chat_responses(message: str, checkpoint_id: Optional[str] = None):
     is_new_conversation = checkpoint_id is None
     
-    # if is_new_conversation:
-    #     # Generate new checkpoint ID for first message in conversation
-    #     new_checkpoint_id = str(uuid4())
+    if is_new_conversation:
+        # Generate new checkpoint ID for first message in conversation
+        new_checkpoint_id = str(uuid4())
 
-    #     config = {
-    #         "configurable": {
-    #             "thread_id": new_checkpoint_id,
-    #             "project_id": "68628d545384d01e747d6b79"
-    #         }
-    #     }
-        
-    #     # Initialize with first message
-    #     events = GRAPHS['chatbot'].astream_events(
-    #         {"messages": [HumanMessage(content=message)]},
-    #         version="v2",
-    #         config=config
-    #     )
-        
-    #     # First send the checkpoint ID
-    #     yield f"data: {{\"type\": \"checkpoint\", \"checkpoint_id\": \"{new_checkpoint_id}\"}}\n\n"
-    # else:
-    if checkpoint_id is None:
-        raise HTTPException(status_code=400, detail="Checkpoint ID is required for existing conversations")
-    config = {
-        "configurable": {
-            "thread_id": checkpoint_id
+        config = {
+            "configurable": {
+                "thread_id": new_checkpoint_id,
+                "project_id": "68628d545384d01e747d6b79"
+            }
         }
-    }
-    # Continue existing conversation
-    events = PROCUREMENT_GRAPHS['chatbot'].astream_events(
-        {"messages": [HumanMessage(content=message)]},
-        version="v2",
-        config=config
-    )
+        
+        # Initialize with first message
+        events = PROCUREMENT_GRAPHS['chatbot'].astream_events(
+            {"messages": [HumanMessage(content=message)]},
+            version="v2",
+            config=config
+        )
+        
+        # First send the checkpoint ID
+        yield f"data: {{\"type\": \"checkpoint\", \"checkpoint_id\": \"{new_checkpoint_id}\"}}\n\n"
+    else:
+        if checkpoint_id is None:
+            raise HTTPException(status_code=400, detail="Checkpoint ID is required for existing conversations")
+        config = {
+            "configurable": {
+                "thread_id": checkpoint_id
+            }
+        }
+        # Continue existing conversation
+        events = PROCUREMENT_GRAPHS['chatbot'].astream_events(
+            {"messages": [HumanMessage(content=message)]},
+            version="v2",
+            config=config
+        )
 
-    async for event in events:
-        event_type = event["event"]
+        async for event in events:
+            event_type = event["event"]
 
-        if event_type in ["on_tool_end","on_tool_start"]:
-            continue
-        if event_type in ["on_chat_model_stream","on_chat_model_end"]:
-            if "chunk" not in event['data']:
+            if event_type in ["on_tool_end","on_tool_start"]:
                 continue
-            
-            chunk_content = serialise_ai_message_chunk(event["data"]["chunk"])
-            safe_content = chunk_content.replace("'", "\\'").replace("\n", "\\n")
-            if safe_content == "":
-                continue
-            yield f"data: {{\"type\": \"content\", \"content\": \"{safe_content}\"}}\n\n"
-    # Send an end event
-    yield f"data: {{\"type\": \"end\"}}\n\n"
+            if event_type in ["on_chat_model_stream","on_chat_model_end"]:
+                if "chunk" not in event['data']:
+                    continue
+                
+                chunk_content = serialise_ai_message_chunk(event["data"]["chunk"])
+                safe_content = chunk_content.replace("'", "\\'").replace("\n", "\\n")
+                if safe_content == "":
+                    continue
+                yield f"data: {{\"type\": \"content\", \"content\": \"{safe_content}\"}}\n\n"
+        # Send an end event
+        yield f"data: {{\"type\": \"end\"}}\n\n"
