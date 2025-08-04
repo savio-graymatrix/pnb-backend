6934481d4f252293dc71996330a6f61c57b67b7e
Savio Fernando <savio.fernando@graymatrix.com>
2025-08-04 12:50:35 +0530

    Fix: Make Amount Optional for Bid

--- pnb/db/data_models/procurement/Bid.py
+++ pnb/db/data_models/procurement/Bid.py
@@ -19,7 +19,7 @@ class Bid(Document):
     series_id: Optional[str] = None
     company: str = Field()
     emd_status: Optional[EMDStatus] = Field(default=EMDStatus.PAID)
-    amount: Decimal = Field(..., gt=0.0, decimal_places=2)
+    amount: Optional[Decimal] = Field(..., default=1.0, gt=0.0, decimal_places=2)
     tender: Link[Tender] = Field()
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
