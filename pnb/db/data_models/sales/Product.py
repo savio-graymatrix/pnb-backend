2248caaf07c9640c8edca8396f74ae8e6c5ee6e8
Savio Fernando <savio.fernando@graymatrix.com>
2025-08-12 15:28:21 +0530

    Fix: Changed name of Data Models

--- pnb/db/data_models/sales/Product.py
+++ pnb/db/data_models/sales/Product.py
@@ -22,3 +22,6 @@ class Product(Document):
     description: str
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    class Settings:
+        name = "product"
