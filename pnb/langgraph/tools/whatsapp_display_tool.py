2a4806bacc5cb0e571590ce308557ffd799d9d2f
Arjun Pathak <arjun.pathak@graymatrix.com>
2025-09-02 15:39:25 +0530

    Fixes: Prompt fixes for the sales agent

--- pnb/langgraph/tools/whatsapp_display_tool.py
+++ pnb/langgraph/tools/whatsapp_display_tool.py
@@ -2,14 +2,14 @@ from langchain.tools import tool
 
 
 @tool
-async def whatsapp_display_tool(markdown_text: str):
+async def whatsapp_display_tool(text: str):
     """
     Tool used to display a whataspp curated message
 
     Args:
-        markdown_text (str): The markdown text to display
+        text (str): The text to display
 
     Returns:
-        str: The markdown text to display
+        str: The text to display
     """
-    return markdown_text
+    return text
