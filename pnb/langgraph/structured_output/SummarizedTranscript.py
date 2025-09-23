07b3b7b5dc95815a5ed3c2fc84ad35837e1af844
Arjun Pathak <arjun.pathak@graymatrix.com>
2025-09-23 18:20:34 +0530

    Fix: Added Action Messages to Transcript Response

--- pnb/langgraph/structured_output/SummarizedTranscript.py
+++ pnb/langgraph/structured_output/SummarizedTranscript.py
@@ -9,7 +9,9 @@ class SummarizedTranscript(BaseModel):
     sentiment: Literal["positive", "negative", "mixed", "neutral"] = Field(
         description="General sentiment of the whole call"
     )
-    next_action: str = Field(description="Next action according to the summary")
+    next_action: Literal[
+        "Escalation Needed", "Call back", "PTP Reminder call", "Query", "Dispute Raised"
+    ] = Field(description="Next action according to the summary")
     next_action_date: str | None = Field(
-        description="Date when will be the next action will be taken. Date is in ISO format in UTC Timezone."
+        description="Date when will be the next action will be taken. Date is in ISO format in UTC Timezone only."
     )
