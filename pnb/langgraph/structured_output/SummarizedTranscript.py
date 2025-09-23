3842626352671318f0d2013a57382e9d725bc50e
Arjun Pathak <arjun.pathak@graymatrix.com>
2025-09-23 18:18:08 +0530

    Fix: Next Action Date in ISO Format

--- pnb/langgraph/structured_output/SummarizedTranscript.py
+++ pnb/langgraph/structured_output/SummarizedTranscript.py
@@ -10,6 +10,6 @@ class SummarizedTranscript(BaseModel):
         description="General sentiment of the whole call"
     )
     next_action: str = Field(description="Next action according to the summary")
-    next_action_date: datetime | None = Field(
-        description="Date when will be the next action will be taken"
+    next_action_date: str | None = Field(
+        description="Date when will be the next action will be taken. Date is in ISO format in UTC Timezone."
     )
