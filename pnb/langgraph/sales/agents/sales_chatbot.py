f38be34e414b063e9cb8c1404a76e81435c4d64d
Arjun Pathak <arjun.pathak@graymatrix.com>
2025-10-09 12:41:01 +0530

    Fix: Sales agent prompt fix

--- pnb/langgraph/sales/agents/sales_chatbot.py
+++ pnb/langgraph/sales/agents/sales_chatbot.py
@@ -68,12 +68,13 @@ class SalesChatbotagent:
                 1. ETB - existing to bank - here the 'customer' collection is to be used along with transaction and communactions collections.
                 2. NTB - new to bank - here the 'lead' collection is to be used.
             - The 'transaction' and 'communications' collections have information of the customer connected thorugh customer_id. The customer_id is present in the customer collection. From the name of the customer, you can get the customer_id from the 'customer' collection and use that to get the data from the 'transaction' and 'communications' collections.
-            - 'customer' collection has name, age, gender, city, occupation, income, segment, credit_score, contact. If asked for recommendation to pitch Gold Coins, use these attributes to recommend customers with reasoning.
+            - 'customer' collection has name, age, gender, city, occupation, income, segment, credit_score, contact.
             - 'transaction' has information about the amount of the transaction, balance - 'balance_after' of the customer.
             - 'communications' has information about the intent, message, bank_response, outcome of the communication. 
             - If you are enquired about transactions or spending patterns of a customer, use the 'transaction' collection to get the data and present your analysis. It is not necessary to show the transaction data explicitly. 
             - if you are enquired about the communication history of a customer, use the 'communications' collection to get the data and present your analysis.
             - The 'lead' collection has information of the leads and the products - 'product' which contains name, type of the product they are interested in and should be curated for them. Any request for leads/lead should be directed here.
+            - You can be tasked to recommend customers who can be pitched/offered Gold Coins/Credit cards/Debit cards/Loans. You need to use the 'customer' collection to get the fields available, create your reasoning and then present your analysis. 
             - You have to create generalized/personalized pitches namely for whatsapp and email to be sent to the customer/lead. The pitches should be catchy and engaging. Use emojis in whatsapp pitches.
             - You also can post on linkedin. Ask permission before posting.
             - You can also create images through imagen tool. The text in the image should strictly be in English. Show the created image by sending in markdown format.
